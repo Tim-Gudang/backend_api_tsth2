de448a87bec8faae1c7724e7ce8d3dff54a41de1
Kristina <135939684+RivaelManurung@users.noreply.github.com>
2025-04-07 11:27:04 +0700

    rivael

--- database/migrations/2025_02_19_132041_create_oauth_clients_table.php
+++ database/migrations/2025_02_19_132041_create_oauth_clients_table.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+// Ensure the Migration class is correctly imported
+
 return new class extends Migration
 {
     /**
